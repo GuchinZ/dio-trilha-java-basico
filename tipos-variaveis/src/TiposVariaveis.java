31b61f3a31a0aec5636007d162fed4424e5ebca6
GuchinZ <guschimith33@gmail.com>
2023-04-08 17:46:20 -0300

    Criação e alimentação do arquivo "TiposVariaveis"

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+
+        String meuNome = "Gustavo Schimith";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        
+        //para que isso seja possível, deve-se adicionar o casting "(short)" para afirmar que o desenvolvedor está ciente do que pode ocorrer com a variável
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //final não permite que o valor da variável seja alterado e o nome da variável, por convenção, deve ser escrito em caixa alta
+        final double VALOR_DE_PI = 3.14;
     }
 }
