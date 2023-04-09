c703c5dfe0c8dc202b62ee8125af29983acb488c
GuchinZ <guschimith33@gmail.com>
2023-04-09 13:13:59 -0300

    Tratando exceções

---
+++ tratamento-excecoes/src/FormatadorDeCepExemplo.java
@@ -0,0 +1,17 @@
+public class FormatadorDeCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep não atende as regras de negócio.");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
